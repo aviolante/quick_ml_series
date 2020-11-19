d6a51ca92f58f5b2101b35a3f779d58b0baa8a28
aviolante <violante.andre@gmail.com>
2020-11-19 11:20:19 -0500

    update show output

--- 01_quick_ml_pca.py
+++ 01_quick_ml_pca.py
@@ -91,15 +91,32 @@ def plot_scree_cumsum_visual(pca_output):
 
 
 # perform pca / svd
-pca_output_sk = principal_component_analysis_sklearn(X_df, n_components=len(X_df.columns))
+pca_output_sk = principal_component_analysis_sklearn(X_df, n_components=len(X_df.columns))  # n-components = n-features
 pca_output_np = principal_component_analysis_numpy_svd(X)
 
 # validate and compare different functions output
 print(pca_output_sk['variance_explained_ratio'])
 print(pca_output_np['variance_explained_ratio'])
 
+# [0.47129606 0.11025193 0.0955859  0.06596732 0.06421661 0.05056978
+#  0.04118124 0.03046902 0.02130333 0.01694137 0.0143088  0.01302331
+#  0.00488533]
+#
+# [0.47129606 0.11025193 0.0955859  0.06596732 0.06421661 0.05056978
+#  0.04118124 0.03046902 0.02130333 0.01694137 0.0143088  0.01302331
+#  0.00488533]
+
+
 print(pca_output_sk['singular_values'])
 print(pca_output_np['singular_values'])
+
+# [55.6793095  26.93022859 25.07516773 20.83105866 20.55278239 18.23864114
+#  16.45874174 14.15716218 11.83779223 10.55653065  9.70171478  9.25566343
+#   5.66883461]
+#
+# [55.6793095  26.93022859 25.07516773 20.83105866 20.55278239 18.23864114
+#  16.45874174 14.15716218 11.83779223 10.55653065  9.70171478  9.25566343
+#   5.66883461]
 
 print(pca_output_sk['principal_components'])
 print(pca_output_np['principal_components'])
@@ -107,7 +124,22 @@ print(pca_output_np['principal_components'])
 print(pd.DataFrame(pca_output_sk['transformed_data']).head())
 print(pd.DataFrame(pca_output_np['transformed_data']).head())
 
+#          0         1         2   ...        10        11        12
+# 0 -2.098297  0.773113  0.342943  ... -0.033000  0.019440  0.365975
+# 1 -1.457252  0.591985 -0.695199  ... -0.640810 -0.125797 -0.070719
+# 2 -2.074598  0.599639  0.167122  ... -0.487557  0.133327 -0.014022
+# 3 -2.611504 -0.006871 -0.100284  ... -0.360209  0.508678  0.007847
+# 4 -2.458185  0.097712 -0.075348  ... -0.395150  0.497732  0.014274
+# [5 rows x 13 columns]
+#
+#          0         1         2   ...        10        11        12
+# 0 -2.098297  0.773113  0.342943  ... -0.033000 -0.019440  0.365975
+# 1 -1.457252  0.591985 -0.695199  ... -0.640810  0.125797 -0.070719
+# 2 -2.074598  0.599639  0.167122  ... -0.487557 -0.133327 -0.014022
+# 3 -2.611504 -0.006871 -0.100284  ... -0.360209 -0.508678  0.007847
+# 4 -2.458185  0.097712 -0.075348  ... -0.395150 -0.497732  0.014274
+# [5 rows x 13 columns]
+
 # plot scree and cumulative sum plots
 plot_scree_cumsum_visual(pca_output_sk)
 plot_scree_cumsum_visual(pca_output_np)
-
